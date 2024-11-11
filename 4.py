43b23a59b819132ec9bd0f9dd1672eda794813e8
Aghifany <101543752+Aghifany@users.noreply.github.com>
2024-11-11 11:31:24 +0700

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,30 @@
+import array as arr
+#
+# myArr = arr.array('i', [1,2,2])
+# print(type(myArr))
+
+# # myCharArr = arr.array('u', ['a', 'b', 'c'])
+# # print(type(myCharArr))
+# #
+# print()
+# myFruitList = ["apple", "banana", "cherry"]
+# print(myFruitList)
+# print(type(myFruitList))
+
+# print(myFruitList[2])
+
+#
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+# print(myFinalAnswerTuple[0])
+# print(myFinalAnswerTuple[1])
+# print(myFinalAnswerTuple[2])
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
